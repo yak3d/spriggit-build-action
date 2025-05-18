0842c83cf4b686b489a66cc6e53b85660e461dc9
yak3d <yak@yak3d.com>
2025-05-18 15:24:59 -0400

    lets try this

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@
  * The entrypoint for the action. This file simply imports and runs the action's
  * main logic.
  */
-import { run } from './main.js'
 
 /* istanbul ignore next */
+import { run } from './main.js'
+
 run()
