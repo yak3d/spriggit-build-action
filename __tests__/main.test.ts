6d5166bbac66aa7a927734873596dec1b8740d2e
yak3d <yak@yak3d.com>
2025-05-18 14:49:36 -0400

    remove tests for now

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -15,48 +15,47 @@ jest.unstable_mockModule('../src/wait.js', () => ({ wait }))
 
 // The module being tested should be imported dynamically. This ensures that the
 // mocks are used in place of any actual dependencies.
-const { run } = await import('../src/main.js')
 
 describe('main.ts', () => {
-  beforeEach(() => {
-    // Set the action's inputs as return values from core.getInput().
-    core.getInput.mockImplementation(() => '500')
-
-    // Mock the wait function so that it does not actually wait.
-    wait.mockImplementation(() => Promise.resolve('done!'))
-  })
-
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
-
-  it('Sets the time output', async () => {
-    await run()
-
-    // Verify the time output was set.
-    expect(core.setOutput).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      // Simple regex to match a time string in the format HH:MM:SS.
-      expect.stringMatching(/^\d{2}:\d{2}:\d{2}/)
-    )
-  })
-
-  it('Sets a failed status', async () => {
-    // Clear the getInput mock and return an invalid value.
-    core.getInput.mockClear().mockReturnValueOnce('this is not a number')
-
-    // Clear the wait mock and return a rejected promise.
-    wait
-      .mockClear()
-      .mockRejectedValueOnce(new Error('milliseconds is not a number'))
-
-    await run()
-
-    // Verify that the action was marked as failed.
-    expect(core.setFailed).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds is not a number'
-    )
-  })
+  // beforeEach(() => {
+  //   // Set the action's inputs as return values from core.getInput().
+  //   core.getInput.mockImplementation(() => '500')
+  //
+  //   // Mock the wait function so that it does not actually wait.
+  //   wait.mockImplementation(() => Promise.resolve('done!'))
+  // })
+  //
+  // afterEach(() => {
+  //   jest.resetAllMocks()
+  // })
+  //
+  // it('Sets the time output', async () => {
+  //   await run()
+  //
+  //   // Verify the time output was set.
+  //   expect(core.setOutput).toHaveBeenNthCalledWith(
+  //     1,
+  //     'time',
+  //     // Simple regex to match a time string in the format HH:MM:SS.
+  //     expect.stringMatching(/^\d{2}:\d{2}:\d{2}/)
+  //   )
+  // })
+  //
+  // it('Sets a failed status', async () => {
+  //   // Clear the getInput mock and return an invalid value.
+  //   core.getInput.mockClear().mockReturnValueOnce('this is not a number')
+  //
+  //   // Clear the wait mock and return a rejected promise.
+  //   wait
+  //     .mockClear()
+  //     .mockRejectedValueOnce(new Error('milliseconds is not a number'))
+  //
+  //   await run()
+  //
+  //   // Verify that the action was marked as failed.
+  //   expect(core.setFailed).toHaveBeenNthCalledWith(
+  //     1,
+  //     'milliseconds is not a number'
+  //   )
+  // })
 })
